054bb110a58f9682db9be8e30470e45ed8554b47
zidan14871 <zidanalamsyah875@gmail.com>
2025-04-19 02:25:31 +0700

    Menambahkan Auth filter dan update config serta sidebar

--- belajar-ci/app/Config/Routes.php
+++ belajar-ci/app/Config/Routes.php
@@ -6,11 +6,11 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
 
 $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
 
-$routes->get('produk', 'ProdukController::index');
-$routes->get('keranjang', 'TransaksiController::index');
+$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
